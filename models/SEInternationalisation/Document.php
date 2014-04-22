e4452c88f751d92d1fa3d64ef0926332049cd66c
Pieter Meyvaert <Pieter@studioemma.com>
2014-04-22 16:32:29 +0200

    Updated SEBasic plugin. Added bootstrap 3.

--- models/SEInternationalisation/Document.php
+++ models/SEInternationalisation/Document.php
@@ -1,12 +1,25 @@
 <?php
-class SEInternationalisation_Document {
-	
-	
-	public static function getDocumentIdInOtherLanguage($document_id, $language) {
+
+/**
+ * Class SEInternationalisation_Document
+ */
+class SEInternationalisation_Document
+{
+
+
+	/**
+	 * @param $document_id
+	 * @param $language
+	 * @return Document
+	 * @throws Exception
+	 * @throws Zend_Db_Table_Exception
+	 */
+	public static function getDocumentIdInOtherLanguage($document_id, $language)
+	{
 		$tSource = new SEInternationalisation_Table_Keys();
 		$tTarget = new SEInternationalisation_Table_Keys();
 		$select = $tSource->select()
-			->from(array("s" =>$tSource->info("name")), array())
+			->from(array("s" => $tSource->info("name")), array())
 			->from(array("t" => $tTarget->info("name")), array("document_id"))
 			->where("s.document_id = ?", $document_id)
 			->where("s.sourcePath = t.sourcePath")
@@ -15,9 +28,8 @@ class SEInternationalisation_Document {
 		if (!empty($row)) {
 			return $row->document_id;
 		} else {
-//			die(print_r(debug_backtrace()));
-			throw new Exception("Document (".$document_id.") not available in ".$language);
+			throw new Exception("Document (" . $document_id . ") not available in " . $language);
 		}
 	}
-	
+
 }
